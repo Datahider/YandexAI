782b7ec4db749a2c5173432954d1556571ffa898
Πέτρος Ιωαννίδης <pio@pio.su>
2025-11-19 12:33:21 +0300

    Добавлен тест асинхронного распознавания (еще не проверен)

--- test/YandexSpeachKitGatewayTest.php
+++ test/YandexSpeachKitGatewayTest.php
@@ -4,6 +4,7 @@ namespace losthost\YandexAI\Test;
 
 use PHPUnit\Framework\TestCase;
 use losthost\YandexAI\YandexSpeachKitGateway;
+use losthost\YandexAI\Operation;
 
 class YandexSpeachKitGatewayTest extends TestCase {
 
@@ -15,6 +16,17 @@ class YandexSpeachKitGatewayTest extends TestCase {
         $text = $stt->recognize(file_get_contents('12345.ogg'));
         $this->assertEquals('1 2 3 4 5 вышел зайчик погулять', $text);
 
+    }
+    
+    public function testSSTAsync() {
+        
+        global $folder_id;
+        
+        $sst = new YandexSpeachKitGateway($folder_id);
+        $operation = $sst->recognizeAsync(file_get_contents('12345.ogg'));
+        var_dump($operation);
+        
+        $this->asertEquals('123', $operation);
     }
     
 }
